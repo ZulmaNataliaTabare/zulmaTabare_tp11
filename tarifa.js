04c163fa6caba91168288946b321959413c92a9a
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-10-25 19:50:03 -0300

    Modifiqué archivos

---
+++ tarifa.js
@@ -0,0 +1,8 @@
+// Desarrolla el programa Ajuste tarifario de energía eléctrica. Utilizando el
+//  operador condicional ternario, queremos determinar cuánto va a pagar un hogar
+//  según su consumo.
+
+let pagoMes = 12000
+
+let consumoKWH = 450
+
