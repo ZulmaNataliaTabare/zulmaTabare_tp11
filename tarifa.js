770f235f395b2bdfb93a7e2c821e7527d1e2d9a1
ZulmaNataliaTabare <sulmatabare@gmail.com>
2024-10-29 13:27:38 -0300

    Modifiqué archivo tarifa.js

--- tarifa.js
+++ tarifa.js
@@ -13,10 +13,12 @@ let consumoKWH = 450;
 let pagoFinal = pagoMes + (pagoMes * 0.20)
 
 let mensaje = (consumoKWH > topeConsumno) ? `"Debido a que su hogar ha tenido un consumo de ${consumoKWH}Kwh, en  base al ajuste
-tarifario (hogares con consumo mayor a ${topeConsumno}Kwh por mes tendrán un aumento del ${aumento}), cumplimos con informarle
-que se ha ajustado el total a pagar, que será de $${pagoFinal}"` :
+tarifario (hogares con consumo mayor a ${topeConsumno}Kwh por mes tendrán un 
+aumento del ${aumento}), cumplimos con informarle que se ha ajustado el total a 
+pagar, que será de $${pagoFinal}"` :
 
 `"Debido a que su hogar ha tenido un consumo de ${consumoKWH}Kwh, en  base al ajuste
-    tarifario (hogares con consumo mayor a ${topeConsumno}Kwh por mes tendrán un aumento del ${aumento}), 
-    cumplimos con informarle que se le seguirá manteniendo el subsidio y el total a pagar será de $${pagoMes}"`;
+    tarifario (hogares con consumo mayor a ${topeConsumno}Kwh por mes tendrán un 
+    aumento del ${aumento}), cumplimos con informarle que se le seguirá manteniendo el subsidio y el total a 
+    pagar será de $${pagoMes}"`;
 console.log(mensaje); 
